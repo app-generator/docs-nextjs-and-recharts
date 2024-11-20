8684e8cc7a1208a07628f3bfdc1bdc15987d2f11
Abdullah Numan <anewman15@hotmail.com>
2024-11-20 17:15:32 +0600

    feat: add useMemoizedChartData util

--- src/app/utils/index.ts
+++ src/app/utils/index.ts
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { format } from "date-fns";
+import { useMemo } from "react";
 
 export const random255Max = () => Math.floor(Math.random() * 255);
 
@@ -7,3 +9,12 @@ export const clockTime = (date: Date) => format(date, "HH:mm");
 export const dateMmmDdYy = (date: Date) => format(date, "MMM dd, yy");
 
 export const dateMmmDd = (date: Date) => format(date, "MMM dd");
+
+export const useMemoizedChartData = (d: any) => {
+  return useMemo(() => {
+    return d?.map((item: any) => ({
+      date: dateMmmDd(new Date(parseInt(item.date))),
+      count: item?.count,
+    }));
+  }, [d]);
+};
