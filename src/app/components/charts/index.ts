3b15ae6c652da2a8379cacc4fe5f84907f78d8d3
Abdullah Numan <anewman15@hotmail.com>
2024-11-20 17:17:42 +0600

    feat: export chart components

---
+++ src/app/components/charts/index.ts
@@ -0,0 +1,4 @@
+export { ResponsiveAreaChart } from "./ResponsiveAreaChart";
+export { ResponsiveBarChart } from "./ResponsiveBarChart";
+export { ResponsiveLineChart } from "./ResponsiveLineChart";
+export { ResponsivePieChart } from "./ResponsivePieChart";
